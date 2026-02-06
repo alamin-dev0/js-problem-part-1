0e84036b009796e65503fc50b5d0e03223acb041
Al Amin <alamin.dev01@gmail.com>
2026-02-07 04:32:41 +0600

    added some code

---
+++ math.js
@@ -0,0 +1,27 @@
+// const Math = {
+//     min: function min(num1, num2) {
+//         return num2;
+//     }
+// }
+// console.log(Math.min)
+// console.log(Math.max)
+
+const min = Math.min(45, 23, 56, 67, 12, 32, 11, 5)
+console.log(min)
+
+const max = Math.max(45, 23, 56, 67, 12, 32, 11, 5, 99)
+console.log(max)
+
+console.log(Math.PI)
+console.log(Math.abs(5 - 10))
+console.log(Math.round(4.5))
+console.log(Math.round(4.4))
+console.log(Math.floor(2.8))
+console.log(Math.floor(2.99999998))
+console.log(Math.ceil(2.00000001))
+
+
+console.log(Math.random())
+console.log(Math.random() * 100)
+const rand = Math.round(Math.random() * 100)
+console.log(rand)
